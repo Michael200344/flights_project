df6a495d7562afbbed97b33d81886118623651d5
Jacob Poeschel <jacobop2@illinois.edu>
2022-11-19 23:59:31 +0000

    necessary files for data simplification, .cpp to be edited later

---
+++ data_simplification.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <string>
+
+
+/*
+
+Data simplification function with the goal of importing a list of desired airports and sorting the data to only include necessary points
+
+Input: Vector list of airport IATAs
+
+Output: populates: Vector of each flight with input IATA as source, vector of each flight with input IATA as destination
+
+*/
+
+void dataSimplification(std::vector<std::string> IATAs){
+
+
+    
+}
