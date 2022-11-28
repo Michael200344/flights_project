50d3e24210c3154fb1d0672b8bcaab22dc15c9a4
Jacob Poeschel <jacobop2@illinois.edu>
2022-11-28 18:00:42 +0000

    src folder

---
+++ flight.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Flight {
+    public:
+    
+
+    private:
+    std::string source;
+    std::string destination;
+    std::string airline;
+    float distance;
+
+};
