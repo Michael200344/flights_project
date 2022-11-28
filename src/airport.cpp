0825822015cc1dfb608ea5d2aebf9fdc3a94397e
Michael Rheintgen <michael.rheintgen@gmail.com>
2022-11-28 18:15:57 +0000

    Airport constructor

--- src/airport.cpp
+++ src/airport.cpp
@@ -1,1 +1,6 @@
 #include "airport.h"
+
+Airport::Airport(string iata, vector<Flight> connections) {
+    IATA = iata;
+    flights = connections;
+}
--- src/airport.h
+++ src/airport.h
@@ -6,7 +6,8 @@
 
 class Airport {
     public:
-    
+    Airport();
+    Airport(string iata, vector<Flight> connections);
 
     private:
     string IATA;
