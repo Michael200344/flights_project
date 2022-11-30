d455445a030a8b0cf1fa3ec773dc06804178c59b
Jacob Poeschel <jacobop2@illinois.edu>
2022-11-30 17:49:26 +0000

    scratch work

--- build/test_tests-b12d07c.cmake
+++ build/test_tests-b12d07c.cmake
@@ -1,3 +1,5 @@
 add_test( [==[Sample Data Adjacency List]==] [==[/workspaces/CS 225/final_project/flights_project/build/test]==] [==[Sample Data Adjacency List]==]  )
 set_tests_properties( [==[Sample Data Adjacency List]==] PROPERTIES WORKING_DIRECTORY [==[/workspaces/CS 225/final_project/flights_project/build]==])
-set( test_TESTS [==[Sample Data Adjacency List]==])
+add_test( [==[tostring test]==] [==[/workspaces/CS 225/final_project/flights_project/build/test]==] [==[tostring test]==]  )
+set_tests_properties( [==[tostring test]==] PROPERTIES WORKING_DIRECTORY [==[/workspaces/CS 225/final_project/flights_project/build]==])
+set( test_TESTS [==[Sample Data Adjacency List]==] [==[tostring test]==])
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,17 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include "adjacency_list.h"
+#include "utils.h"
 
 using namespace std;
 
 TEST_CASE("Sample Data Adjacency List", "[weight=5]") {
 
     cout << "here" << endl;
+
+    ifstream myfile;
+    myfile.open("used_data.csv");
+
     AdjList adj = *(new AdjList("used_data.csv"));
 
     for(const Airport &a : adj.getList()){
@@ -16,6 +21,13 @@ TEST_CASE("Sample Data Adjacency List", "[weight=5]") {
 
     }
 
+    myfile.close();
+
     REQUIRE(1 == 1);
-    
+}
+
+TEST_CASE("tostring test") {
+
+    cout << file_to_string("test_out.csv");
+
 }
